cead6963b6040671a6a94792381427b9f9089e66
Liu Xianning <xianlinbox@gmail.com>
2016-05-12 22:07:25 +0800

    finish pascal algorithm

--- src/main/scala/recfun/Main.scala
+++ src/main/scala/recfun/Main.scala
@@ -1,5 +1,4 @@
 package recfun
-import common._
 
 object Main {
   def main(args: Array[String]) {
@@ -12,17 +11,37 @@ object Main {
   }
 
   /**
-   * Exercise 1
-   */
-  def pascal(c: Int, r: Int): Int = ???
+    * Exercise 1
+    */
+  def pascal(c: Int, r: Int): Int = {
+    def buildTriangle(): Array[Array[Int]] = {
+      val triangle: Array[Array[Int]] = Array.ofDim[Int](r + 1, r + 1)
+
+      for (i <- 1 to r) {
+        val currentRow = Array.ofDim[Int](i + 1)
+        currentRow(0) = 1
+        currentRow(i) = 1
+
+        for (j <- 1 until i) {
+          println("j:" + j + " i:" + i)
+          currentRow(j) = triangle(i - 1)(j) + triangle(i - 1)(j - 1)
+        }
+        triangle(i) = currentRow
+      }
+      triangle
+    }
+
+    buildTriangle()(r)(c)
+  }
+
 
   /**
-   * Exercise 2
-   */
-  def balance(chars: List[Char]): Boolean = ???
+    * Exercise 2
+    */
+  def balance(chars: List[Char]): Boolean = true
 
   /**
-   * Exercise 3
-   */
-  def countChange(money: Int, coins: List[Int]): Int = ???
+    * Exercise 3
+    */
+  def countChange(money: Int, coins: List[Int]): Int = 0
 }
--- src/test/scala/recfun/PascalSuite.scala
+++ src/test/scala/recfun/PascalSuite.scala
@@ -19,4 +19,12 @@ class PascalSuite extends FunSuite {
   test("pascal: col=1,row=3") {
     assert(pascal(1,3) === 3)
   }
+
+  test("pascal: col=0,row=4") {
+    assert(pascal(0,4) === 1)
+  }
+
+  test("pascal: col=2,row=4") {
+    assert(pascal(2,4) === 6)
+  }
 }
