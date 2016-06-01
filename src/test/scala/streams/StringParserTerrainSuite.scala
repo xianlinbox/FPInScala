36cbd07637e48a337759b70d1a554179c7f9ab12
Liu Xianning <xianlinbox@gmail.com>
2016-06-01 22:25:37 +0800

    add stringParser tests

---
+++ src/test/scala/streams/StringParserTerrainSuite.scala
@@ -0,0 +1,42 @@
+package streams
+
+import org.junit.runner.RunWith
+import org.scalatest.FunSuite
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class StringParserTerrainSuite extends FunSuite {
+
+  trait Level1 extends GameDef with StringParserTerrain{
+    val level =
+      """ooo-------
+        |oSoooo----
+        |oT-""".stripMargin
+
+    val levelVector = Vector(Vector('o', 'o','o'), Vector('o', 'o','S', 'o'), Vector('o', 'T'))
+  }
+
+	test("terrainFunction: Pos(0,0) => true") {
+    new Level1 {
+      assert(terrainFunction(levelVector)(Pos(0,0)))
+    }
+  }
+
+  test("terrainFunction: Pos(1,3) => true") {
+    new Level1 {
+      assert(terrainFunction(levelVector)(Pos(1,3)))
+    }
+  }
+
+  test("terrainFunction: Pos(0,3) => false") {
+    new Level1 {
+      assert(!terrainFunction(levelVector)(Pos(0,3)))
+    }
+  }
+
+  test("terrainFunction: Pos(4,0) => false") {
+    new Level1 {
+      assert(!terrainFunction(levelVector)(Pos(4,0)))
+    }
+  }
+}
